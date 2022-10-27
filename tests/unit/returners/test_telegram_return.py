3ddbaaecb35aa4259708362969fa0c84771f9f40
MKLeb <calebb@vmware.com>
2022-10-26 20:20:21 -0400

    Migrate returners unit tests to pytest

--- tests/unit/returners/test_telegram_return.py
+++ tests/unit/returners/test_telegram_return.py
@@ -1,43 +1,37 @@
 """
+    Test Telegram Returner
+
     :codeauthor: :email:`Roald Nefs (info@roaldnefs.com)`
-
-    tests.unit.returners.telegram_return_test
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
-
+import pytest
 
 import salt.returners.telegram_return as telegram
-from tests.support.mixins import LoaderModuleMockMixin
 from tests.support.mock import MagicMock, patch
-from tests.support.unit import TestCase
 
 
-class TelegramReturnerTestCase(TestCase, LoaderModuleMockMixin):
+@pytest.fixture
+def configure_loader_modules():
+    return {telegram: {}}
+
+
+def test_returner():
     """
-    Test Telegram Returner
+    Test to see if the Telegram returner sends a message
     """
+    ret = {
+        "id": "12345",
+        "fun": "mytest.func",
+        "fun_args": "myfunc args",
+        "jid": "54321",
+        "return": "The room is on fire as shes fixing her hair",
+    }
+    options = {"chat_id": "", "token": ""}
 
-    def setup_loader_modules(self):
-        return {telegram: {}}
-
-    def test_returner(self):
-        """
-        Test to see if the Telegram returner sends a message
-        """
-        ret = {
-            "id": "12345",
-            "fun": "mytest.func",
-            "fun_args": "myfunc args",
-            "jid": "54321",
-            "return": "The room is on fire as shes fixing her hair",
-        }
-        options = {"chat_id": "", "token": ""}
-
-        with patch(
-            "salt.returners.telegram_return._get_options",
-            MagicMock(return_value=options),
-        ), patch.dict(
-            "salt.returners.telegram_return.__salt__",
-            {"telegram.post_message": MagicMock(return_value=True)},
-        ):
-            self.assertTrue(telegram.returner(ret))
+    with patch(
+        "salt.returners.telegram_return._get_options",
+        MagicMock(return_value=options),
+    ), patch.dict(
+        "salt.returners.telegram_return.__salt__",
+        {"telegram.post_message": MagicMock(return_value=True)},
+    ):
+        assert telegram.returner(ret) is True
